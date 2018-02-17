a4f615e19d3a889d31215a02dc94d2553f623ec5
tmartins1 <tiagomartins@Tiagos-Air.lan>
2018-02-17 16:04:11 +0000

    moved Bar to seperated component

--- src/Router.js
+++ src/Router.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import { Dimensions } from 'react-native';
 import { Scene, Router } from 'react-native-router-flux';
-import LinearGradient from 'react-native-linear-gradient';
-import MCIIcon from 'react-native-vector-icons/Entypo';
 import MIIcon from 'react-native-vector-icons/MaterialIcons';
 import LoginForm from './components/LoginForm';
 import MainMenu from './components/MainMenu';
 import SignupForm from './components/SignupForm';
-
-const Title = () => {
-  return (
-    <LinearGradient
-      colors={['#0776B6', '#3BC0C1']}
-      style={{
-        position: 'absolute',
-        flex: 1,
-        width: Dimensions.get('window').width,
-        height: 81,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-      start={{x: 0.0, y: 0.5}} end={{x: 1, y: 0.5}}
-    >
-      <MCIIcon name="home" size={50} color="white"
-        style={{bottom: 8}}/>
-    </LinearGradient>
-  );
-};
+import { Bar } from './components/common';
 
 const RouterComponent = () => {
   return (
@@ -38,12 +17,14 @@ const RouterComponent = () => {
           key="mainMenu"
           component={MainMenu}
           navigationBarStyle={styles.barStyle}
-          renderTitle={() => { return <Title />; }}
+          renderTitle={() => { return <Bar/>; }}
           renderBackButton={() =>
-              <MIIcon name="keyboard-arrow-left" size={55} color="white"
+              <MIIcon
+                name="keyboard-arrow-left"
+                size={55}
+                color="white"
                 style={{bottom: 8}}/>
           }
-
           back
           initial
         />
