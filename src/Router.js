89901341ad31cfaca225933f6590968712a9b23a
tmartins1 <tiagomartins@Tiagos-Air.lan>
2018-02-17 08:28:04 +0000

    added navBar

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,64 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
 import { Scene, Router } from 'react-native-router-flux';
+import LinearGradient from 'react-native-linear-gradient';
+import MCIIcon from 'react-native-vector-icons/Entypo';
+import MIIcon from 'react-native-vector-icons/MaterialIcons';
 import LoginForm from './components/LoginForm';
 import MainMenu from './components/MainMenu';
 import SignupForm from './components/SignupForm';
 
+const Title = () => {
+  return (
+    <LinearGradient
+      colors={['#0776B6', '#3BC0C1']}
+      style={{
+        position: 'absolute',
+        flex: 1,
+        width: Dimensions.get('window').width,
+        height: 81,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+      start={{x: 0.0, y: 0.5}} end={{x: 1, y: 0.5}}
+    >
+      <MCIIcon name="home" size={50} color="white"
+        style={{bottom: 8}}/>
+    </LinearGradient>
+  );
+};
+
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene hideNavBar>
-        <Scene key="login" component={LoginForm}/>
+      <Scene >
+        <Scene key="login" component={LoginForm} hideNavBar/>
         <Scene key="signup" component={SignupForm}/>
-        <Scene key="mainMenu" component={MainMenu}/>
+        <Scene
+          key="mainMenu"
+          component={MainMenu}
+          navigationBarStyle={styles.barStyle}
+          renderTitle={() => { return <Title />; }}
+          renderBackButton={() =>
+              <MIIcon name="keyboard-arrow-left" size={55} color="white"
+                style={{bottom: 8}}/>
+          }
+
+          back
+          initial
+        />
       </Scene>
     </Router>
   );
 };
+
+var styles = {
+  barStyle: {
+    position: 'absolute',
+    alignSelf: 'center',
+    top: Dimensions.get('window').height*0.91,
+    width: Dimensions.get('window').width,
+  },
+}
 
 export default RouterComponent;
